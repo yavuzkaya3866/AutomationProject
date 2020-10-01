d6dd83c6269dfe98802d6b724eed2bf0c5a0392c
Yavuz Kaya <yavuzkaya3866@gmail.com>
2020-10-01 18:10:43 +0200

    last change

---
+++ src/main/java/ForGit.java
@@ -0,0 +1,12 @@
+public class ForGit {
+
+    public static void main(String[] args) {
+
+        String str= "Hello ";
+        String str2 = "world";
+        String str3= str.concat(str2);
+
+        System.out.println(str3);
+    }
+
+}
